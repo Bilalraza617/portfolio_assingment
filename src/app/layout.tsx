4ccd88cf5fa77ab4224bf707cf559f29b65e5316
Bilal raza <anwarbilal617@gmail.com>
2024-11-10 23:40:54 +0500

    complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import { Kushan } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const kushan = Kushan({
+//   weight: '400',
+//   subsets: ['latin'],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
